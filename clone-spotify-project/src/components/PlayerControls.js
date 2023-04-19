7480977680eead2430ba45c6810c4d93ff5135f4
bobbb2912 <nthien2912@gmail.com>
2023-04-19 16:53:31 +0700

    update current track

--- clone-spotify-project/src/components/PlayerControls.js
+++ clone-spotify-project/src/components/PlayerControls.js
@@ -3,23 +3,67 @@ import playerControlsStyles from '../css/PlayerControls.module.css'
 import {BsFillPlayCircleFill, BsFillPauseCircleFill, BsShuffle} from 'react-icons/bs'
 import {CgPlayTrackNext, CgPlayTrackPrev} from 'react-icons/cg'
 import {FiRepeat} from 'react-icons/fi'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import axios from 'axios'
+import { getCurrentTrack } from '../features/login/loginSlice'
 
 export default function PlayerControls() {
     const loginReducer = useSelector((state) => state.loginReducer);
+    const dispatch = useDispatch();
+
+    const changeTrack = async (type) => {
+        await axios.post(
+            `https://api.spotify.com/v1/me/player/${type}`, 
+            {},
+            {
+            headers : {
+                Authorization: "Bearer " + loginReducer.token,
+                "Content-Type": "application/json",
+            },
+        });
+
+        const response = await axios.get(
+            "https://api.spotify.com/v1/me/player/currently-playing", 
+            {
+            headers : {
+                Authorization: "Bearer " + loginReducer.token,
+                "Content-Type": "application/json",
+            },
+        });
+       
+        console.log('curentTrack',response);
+        if(response.data !=="") {
+            const {item} = response.data;
+            const currentlyPlaying = {
+                id: item.id,
+                name: item.name,
+                artists: item.artists.map((artists) => artists.name),
+                image: item.album.images[2].url,
+
+            };
+            dispatch(getCurrentTrack(currentlyPlaying));
+
+        } else {
+            dispatch(getCurrentTrack(null));
+        }
+
+        console.log('player controls', response);
+        // dispatch();
+       
+    }
   return (
     <div className={playerControlsStyles.playerControls}>
        <div className={playerControlsStyles.shuffle}>
             <BsShuffle/>
         </div> 
         <div className={playerControlsStyles.previous}>
-            <CgPlayTrackPrev/>
+            <CgPlayTrackPrev onClick={()=>changeTrack("previous")}/>
         </div>
         <div className={playerControlsStyles.state}>
             {loginReducer.playerState ? <BsFillPauseCircleFill/> : <BsFillPlayCircleFill/>}
         </div>
         <div className={playerControlsStyles.next}>
-            <CgPlayTrackNext/>
+            <CgPlayTrackNext onClick={()=>changeTrack("next")}/>
         </div>
         <div className={playerControlsStyles.repeat}>
             <FiRepeat/>
